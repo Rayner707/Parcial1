8001a8681b0e167ae6f7697af0a599ffb106e015
Rayner Jimmy Tapia Caballero <a2019116944@estudiantes.upsa.edu.bo>
2021-03-20 07:28:24 -0700

    Creacion del modelo

---
+++ Parcial1_RaynerJimmyTapiaCaballero/Parcial1_RaynerJimmyTapiaCaballero/Models/Products.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Parcial1_RaynerJimmyTapiaCaballero.Models
+{
+    public class Products
+    {
+        [Key]
+
+
+        public int ProductID { get; set; }
+
+        [Required]
+        [StringLength(30,ErrorMessage ="Ingrese una descripcion de entre 3 a 30 caracteres",MinimumLength =3)]
+        
+        public string Description { get; set; }
+        [Required]
+        [DataType(DataType.Currency)]
+        public int Price { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}",ApplyFormatInEditMode =true)]
+        public DateTime LastBuy { get; set; }
+    }
+}
