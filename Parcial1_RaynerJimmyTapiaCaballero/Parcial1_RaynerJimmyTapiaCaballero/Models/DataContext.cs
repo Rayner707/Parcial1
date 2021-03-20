76c89170f70b5b1f653fa7941d38fd8b73568817
Rayner Jimmy Tapia Caballero <a2019116944@estudiantes.upsa.edu.bo>
2021-03-20 08:10:30 -0700

    Controlador de Productos Agregado

---
+++ Parcial1_RaynerJimmyTapiaCaballero/Parcial1_RaynerJimmyTapiaCaballero/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Parcial1_RaynerJimmyTapiaCaballero.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<Parcial1_RaynerJimmyTapiaCaballero.Models.Products> Products { get; set; }
+    }
+}
