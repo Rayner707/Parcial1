061e49803ecec524168fcdb75e6ec98a09de9b1b
Rayner Jimmy Tapia Caballero <a2019116944@estudiantes.upsa.edu.bo>
2021-03-20 09:00:34 -0700

    Controlador suma faltante(tres intentos fallidos)

---
+++ Parcial1_RaynerJimmyTapiaCaballero/Parcial1_RaynerJimmyTapiaCaballero/Controllers/CalculatorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Parcial1_RaynerJimmyTapiaCaballero.Controllers
+{
+    public class CalculatorController : ApiController
+    {
+        public int Add(int numa, int numb)
+        {
+            return numa + numb;
+        }
+    }
+}
